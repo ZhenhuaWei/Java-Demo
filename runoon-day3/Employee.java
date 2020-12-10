95e0fb146078c313c488303d8048c99850bd0fa6
weizhenhua <weizhenhua94@163.com>
2020-12-10 09:54:29 +0800

    update java value demo

--- runoon-day3/Employee.java
+++ runoon-day3/Employee.java
@@ -1,4 +1,5 @@
 import java.io.*;
+/*
 public class Employee {
     //这个实例变量对子类可见
     public String name;
@@ -25,4 +26,29 @@ public class Employee {
         empOne.setSalary(5000.0);
         empOne.printEmp();
     } 
+}
+*/
+
+public class Employee{//类名称首字母大写
+    //salary是静态的私有变量
+    //private 是说不能被继承; 
+    //static  是说该静态变量可以直接通过类名来访问，也可以实例化对象后通过对象进行访问 
+    private static double salary;
+
+    //DEPARTMENT是一个常量
+    public static final String DEPARTMENT="开发人员";//常量需要全部大写
+
+    public static void printSalary(){//方法首字母不大写，紧接的单词首字母要大写
+        System.out.println("实际薪资："+ salary);
+    }
+
+    public static void main(String[] args){
+        salary = 10000;//变量不要大写
+        System.out.println(DEPARTMENT+"工资："+ salary);
+
+        Employee.salary = 30000;//不需要实例化类，直接修改static静态变量，后直接可以使用
+        System.out.println("升职加薪后："+Employee.salary);
+        Employee.printSalary();
+    }
+
 }
