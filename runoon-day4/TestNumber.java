68213f69bfd0f6930633a10a87fe12db462674f2
weizhenhua <weizhenhua94@163.com>
2020-12-17 11:08:48 +0800

    update Math class demo

--- runoon-day4/TestNumber.java
+++ runoon-day4/TestNumber.java
@@ -15,5 +15,18 @@ public class TestNumber{
         System.out.println("1的正反切值："+Math.atan(1));
         System.out.println("PI/2的角度值"+Math.toDegrees(Math.PI/2));
         System.out.println(Math.PI);
+
+        double[] nums = {1.4, 1.5, 1.6, -1.4, -1.5, -1.6};
+        for(double num: nums){
+            test(num);
+        }
+    }
+
+    //试一试main函数在上 实现方法在下这这样的调用方式，c语言的化会报错
+    public static void test(double num){
+        System.out.println("Math.floor(" + num +")=" + Math.floor(num));
+        System.out.println("Math.round(" + num +")=" + Math.round(num));
+        System.out.println("Math.ceil(" + num +")=" + Math.ceil(num));
+        System.out.println();
     }
 }
