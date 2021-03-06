d31df43363e4b475ece3cfaaa1643d5d3429728c
deepin <weizhenhua94@163.com>
2021-03-07 01:14:28 +0800

    test extends

---
+++ fkjava/code-2/Apple.java
@@ -0,0 +1,12 @@
+public class Apple extends Fruit
+{
+    public static void main(String[] args) {
+        Apple a = new Apple();
+        a.weight = 56;
+        a.info();
+    }
+}
+
+//编译方法
+//javac Apple.java
+//java Apple
